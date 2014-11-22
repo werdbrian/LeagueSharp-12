dfc1b45c0392ad73941578faed0f85c51c34a29f
E2Slayer <english00412@gmail.com>
2014-11-21 19:38:03 -0500

    Added TargetSelector
    Added xSLx TargetSelector by request.

--- xSLx-Orbwalker Standalone/xSLx-Orbwalker Standalone/Program.cs
+++ xSLx-Orbwalker Standalone/xSLx-Orbwalker Standalone/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
 using xSLx_Orbwalker;
+using xSLx_TargetSelector;
 
 namespace xSLx_Orbwalker_Standalone
 {
@@ -19,11 +20,19 @@ namespace xSLx_Orbwalker_Standalone
         public static void Game_OnGameLoad(EventArgs args)
         {
             Game.PrintChat("<font color='#FF0000'>xSLx Orbwalker standalone</font> loaded. - <font color='#5882FA'>E2Slayer</font>");
+
+            //xSLxOrbwalker Load part
             var menu = new Menu("xSLx Orbwalker", "my_mainmenu", true);
             var orbwalkerMenu = new Menu("xSLx Orbwalker", "my_Orbwalker");
             xSLxOrbwalker.AddToMenu(orbwalkerMenu);
             menu.AddSubMenu(orbwalkerMenu);
             menu.AddToMainMenu();
+
+
+            //xSLxActivator Load part
+            var targetselectormenu = new Menu("TargetSelector", "Common_TargetSelector");
+            SimpleTs.AddToMenu(targetselectormenu);
+            menu.AddSubMenu(targetselectormenu);
         }
     }
 }
