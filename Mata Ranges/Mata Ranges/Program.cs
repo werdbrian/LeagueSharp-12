3e9d17a2a541229ed1049b579eecb136766c6810
E2Slayer <english00412@gmail.com>
2015-01-10 12:29:30 -0500

    Mata Ranges add

---
+++ Mata Ranges/Mata Ranges/Program.cs
@@ -0,0 +1,31 @@
+﻿#region
+
+using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+#endregion
+
+
+namespace Mata_Ranges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Ranges.RangesMenu();
+            Chat("1.0.0.0");
+        }
+
+        private static void Chat(string message)
+        {
+            Game.PrintChat("<font color=\"#4EE2EC\">Mata Ranges</font> - " + message + " v - <font color=\"#B6B6B4\">E2Slayer</font>");
+        }
+
+    }
+}
---
+++ Mata Ranges/Mata Ranges/Ranges.cs
@@ -0,0 +1,206 @@
+﻿#region
+
+using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+#endregion
+
+
+namespace Mata_Ranges
+{
+    public class Ranges
+    {
+        public static Menu RangesM;
+
+        private static Obj_AI_Hero Player = ObjectManager.Player;
+        private static Menu _turretrange;
+        private static MenuItem _turretrangeAlly, _turretrangeEnemy, _turretrangeMax, _turretrangeQu, _turretrangeThick, _turretrangeActive;
+
+        private static Menu _shoprange;
+        private static MenuItem _shoprangeColor, _shoprangeMax, _shoprangeQu, _shoprangeThick, _shoprangeActive;
+
+        private static Menu _aaRange;
+        private static MenuItem _myaaRange, _enemyaarangeOn, _enemyaarangeOff, _aaQu, _aaThick, _aarangeActive;
+
+        public static void RangesMenu()
+        {
+            RangesM = new Menu("Mata Ranges", "Mata Ranges", true);
+
+
+            _turretrange = new Menu("Turret Range", "Turretrange");
+            _turretrangeAlly = new MenuItem("AllyTurret", "Ally Turret Range").SetValue(new Circle(true, Color.Lime));
+            _turretrangeEnemy = new MenuItem("EnemyTurret", "Enemy Turret Range").SetValue(new Circle(true, Color.Red));
+            _turretrangeThick = new MenuItem("Quality", "Range Editor [Default = 0]").SetValue(new Slider(0, 100, -100));
+            _turretrangeMax = new MenuItem("MaxRange", "Max Range [Default = 2000]").SetValue(new Slider(2000, 8000, 100));
+            _turretrangeQu = new MenuItem("Thickness", "Circle Thickness [Default = 5]").SetValue(new Slider(5, 20, 1));
+            _turretrangeActive = new MenuItem("ActiveTurret", "Active Turret Range").SetValue(false);
+
+            _shoprange = new Menu("Shop Range", "shoprange");
+            _shoprangeColor = new MenuItem("Shoprange", "Shop Range").SetValue(new Circle(true, Color.DodgerBlue));
+            _shoprangeThick = new MenuItem("SQuality", "Range Editor [Default = 0]").SetValue(new Slider(0, 100, -100));
+            _shoprangeMax = new MenuItem("ShopMaxRange", "Max Range [Default = 2000]").SetValue(new Slider(2000, 8000, 100));
+            _shoprangeQu = new MenuItem("SThickness", "Circle Thickness [Default = 5]").SetValue(new Slider(5, 20, 1));
+            _shoprangeActive = new MenuItem("ActiveShop", "Active Shop Range").SetValue(false);
+
+            _aaRange = new Menu("AA Ranges", "aaranges");
+            _myaaRange = new MenuItem("Myrange", "My AA Range").SetValue(new Circle(true, Color.Lime));
+            _enemyaarangeOn = new MenuItem("EnemyAAon", "Enemy AA out Range").SetValue(new Circle(true, Color.Lime));
+            _enemyaarangeOff = new MenuItem("EnemyAAoff", "Enemy AA in Range").SetValue(new Circle(true, Color.Red));
+            _aaThick = new MenuItem("AAQuality", "Range Editor [Default = 0]").SetValue(new Slider(0, 100, -100));
+            _aaQu = new MenuItem("AAThickness", "Circle Thickness [Default = 5]").SetValue(new Slider(5, 20, 1));
+            _aarangeActive = new MenuItem("AAactive", "Active Show AA Ranges").SetValue(false);
+
+            RangesM.AddSubMenu(_turretrange);
+            RangesM.SubMenu("Turretrange").AddItem(_turretrangeAlly);
+            RangesM.SubMenu("Turretrange").AddItem(_turretrangeEnemy);
+            RangesM.SubMenu("Turretrange").AddItem(_turretrangeThick);
+            RangesM.SubMenu("Turretrange").AddItem(_turretrangeMax);
+            RangesM.SubMenu("Turretrange").AddItem(_turretrangeQu);
+            RangesM.SubMenu("Turretrange").AddItem(_turretrangeActive);
+
+            RangesM.AddSubMenu(_shoprange);
+            RangesM.SubMenu("shoprange").AddItem(_shoprangeColor);
+            RangesM.SubMenu("shoprange").AddItem(_shoprangeThick);
+            RangesM.SubMenu("shoprange").AddItem(_shoprangeMax);
+            RangesM.SubMenu("shoprange").AddItem(_shoprangeQu);
+            RangesM.SubMenu("shoprange").AddItem(_shoprangeActive);
+
+            RangesM.AddSubMenu(_aaRange);
+            RangesM.SubMenu("aaranges").AddItem(_myaaRange);
+            RangesM.SubMenu("aaranges").AddItem(_enemyaarangeOn);
+            RangesM.SubMenu("aaranges").AddItem(_enemyaarangeOff);
+            RangesM.SubMenu("aaranges").AddItem(_aaThick);
+            RangesM.SubMenu("aaranges").AddItem(_aaQu);
+            RangesM.SubMenu("aaranges").AddItem(_aarangeActive);
+
+            RangesM.AddToMainMenu();
+            Drawing.OnDraw += Drawing_OnDraw;
+
+        }
+
+        public static void DrawingAA()
+        {
+            var aaActive = RangesM.Item("AAactive").GetValue<bool>();
+
+            if (!aaActive) return;
+
+            var MyAARange = RangesM.Item("Myrange").GetValue<Circle>();
+            var EnemyAAOn = RangesM.Item("EnemyAAon").GetValue<Circle>();
+            var EnemyAAOff = RangesM.Item("EnemyAAoff").GetValue<Circle>();
+            var AAThick = RangesM.Item("AAThickness").GetValue<Slider>().Value;
+            var AAQual = RangesM.Item("AAQuality").GetValue<Slider>().Value;
+
+
+
+            if (MyAARange.Active)
+                Utility.DrawCircle(Player.Position, Orbwalking.GetRealAutoAttackRange(Player) + AAQual, MyAARange.Color, AAThick,
+                    AAQual);
+
+            foreach (var target in ObjectManager.Get<Obj_AI_Hero>().Where(target => target.IsValidTarget(2000))) // coded by Trees
+            {
+                var realAARange = target.AttackRange + target.BoundingRadius + AAQual;
+
+                if (EnemyAAOn.Active && EnemyAAOff.Active)
+                {
+                    Utility.DrawCircle(target.Position, realAARange, Player.Distance(target) >= realAARange ? EnemyAAOn.Color : EnemyAAOff.Color, AAThick, AAQual);
+                }
+                else if (EnemyAAOn.Active)
+                {
+                    Utility.DrawCircle(target.Position, realAARange, EnemyAAOn.Color, AAThick, AAQual);
+                }
+                else if (EnemyAAOff.Active)
+                {
+                    Utility.DrawCircle(target.Position, realAARange, Player.Distance(target) >= realAARange ? Color.Empty : EnemyAAOff.Color, AAThick, AAQual);
+                }
+
+
+            }
+
+        }
+
+        public static void DrawingTurret()
+        {
+            var ActiveTcheck = RangesM.Item("ActiveTurret").GetValue<bool>();
+            var AllyTurret = RangesM.Item("AllyTurret").GetValue<Circle>().Active;
+            var EnemyTurret = RangesM.Item("EnemyTurret").GetValue<Circle>().Active;
+
+            if (!ActiveTcheck || (!AllyTurret && !EnemyTurret)) return;
+
+            var CThick = RangesM.Item("Thickness").GetValue<Slider>().Value;
+            var CQuality = RangesM.Item("Quality").GetValue<Slider>().Value;
+            var TurretDistance = RangesM.Item("MaxRange").GetValue<Slider>().Value;
+
+            foreach (Obj_AI_Turret turret in ObjectManager.Get<Obj_AI_Turret>().Where(turret => turret.IsVisible && !turret.IsDead && turret.IsValid && ObjectManager.Player.ServerPosition.Distance(turret.ServerPosition) < TurretDistance && (turret.IsAlly && AllyTurret || turret.IsEnemy && EnemyTurret)))
+            {
+                Utility.DrawCircle(turret.Position, 900f + CQuality, RangesM.Item(turret.IsAlly ? "AllyTurret" : "EnemyTurret").GetValue<Circle>().Color, CThick);
+            }
+
+        }
+
+        public static void DrawingShop()
+        {
+            var ActiveShop = RangesM.Item("ActiveShop").GetValue<bool>();
+            var ShopRange = RangesM.Item("Shoprange").GetValue<Circle>().Active;
+            var Map = Utility.Map.GetMap();
+
+            if (!ActiveShop || !ShopRange || Map == null) return;
+
+            var SThick = RangesM.Item("SThickness").GetValue<Slider>().Value;
+            var SQuality = RangesM.Item("SQuality").GetValue<Slider>().Value;
+            var shopRangeD = 1050;
+            var ShopP = new Vector3();
+
+            foreach (
+                 var shop in
+                     ObjectManager.Get<Obj_Building>().Where(shop => shop.IsAlly && shop.IsValid && ObjectManager.Player.Team == shop.Team && ObjectManager.Player.ServerPosition.Distance(shop.Position) < RangesM.Item("ShopMaxRange").GetValue<Slider>().Value))
+            {
+                if (Map.Type == Utility.Map.MapType.SummonersRift && shop.Name.Contains("__Spawn_T"))
+                {
+                    ShopP = new Vector3(shop.Position.X, shop.Position.Y, shop.Position.Z - 100f);
+                    Utility.DrawCircle(ShopP, shopRangeD + SQuality, RangesM.Item("Shoprange").GetValue<Circle>().Color, SThick);
+                }
+
+                else if (Map.Type != Utility.Map.MapType.SummonersRift && shop.Name.Contains("rderShop"))
+                {
+
+                    switch (Map.Type)
+                    {
+                        case Utility.Map.MapType.TwistedTreeline:
+                            shopRangeD = 1250 + SQuality;
+                            ShopP = new Vector3(shop.Position.X, shop.Position.Y, shop.Position.Z - 100f);
+                            break;
+                        case Utility.Map.MapType.HowlingAbyss:
+                            shopRangeD = 1250 + SQuality;
+                            ShopP = new Vector3(shop.Position.X, shop.Position.Y, shop.Position.Z - 100f);
+                            break;
+                        case Utility.Map.MapType.CrystalScar:
+                            shopRangeD = 1200 + SQuality;
+                            ShopP = new Vector3(shop.Position.X, shop.Position.Y, shop.Position.Z - 200f);
+                            break;
+                    }
+
+                    Utility.DrawCircle(ShopP, shopRangeD + SQuality, RangesM.Item("Shoprange").GetValue<Circle>().Color, SThick);
+                }
+
+            }
+
+
+
+
+        }
+
+
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (ObjectManager.Player.IsDead) return;
+            DrawingTurret();
+            DrawingShop();
+            DrawingAA();
+        }
+    }
+}
