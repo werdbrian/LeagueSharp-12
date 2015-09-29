c9095e432d156cdfc7f552912460e3e95aa4072e
werdbrian <werdbrian@gmail.com>
2015-09-28 20:20:30 -0700

    Update Program.cs

--- Mata Indicator/Mata Indicator/Program.cs
+++ Mata Indicator/Mata Indicator/Program.cs
@@ -70,6 +70,7 @@ namespace Mata_Indicator
             {
                 var dis = (int)Player.Distance(Game.CursorPos);
                 Champ.Item("Range" + slotnumber + Player.ChampionName).SetValue(new Slider(dis, 100, 2000));
+                Champ.Item("Angle" + slotnumber + Player.ChampionName).SetValue(new Slider(dis, 0, 180));
             }
         }
 
@@ -91,8 +92,18 @@ namespace Mata_Indicator
                         }
                         else
                         {
-                            var line = new Geometry.Polygon.Line(ObjectManager.Player.Position, Game.CursorPos, Champ.Item("Range" + slot.Key + Player.ChampionName).GetValue<Slider>().Value);
-                            line.Draw(Champ.Item("Color" + slot.Key + Player.ChampionName).GetValue<Color>(), Champ.Item("Width" + slot.Key + Player.ChampionName).GetValue<Slider>().Value);
+                            var coneAngle = Champ.Item("Angle" + slot.Key + Player.ChampionName).GetValue<Slider>().Value;
+                            var abilRange = Champ.Item("Range" + slot.Key + Player.ChampionName).GetValue<Slider>().Value;
+                            var drawColor = Champ.Item("Color" + slot.Key + Player.ChampionName).GetValue<Color>;
+                            var lineWidth = Champ.Item("Width" + slot.Key + Player.ChampionName).GetValue<Slider>().Value;
+                           var line = new Geometry.Polygon.Line(ObjectManager.Player.Position, Game.CursorPos, abilRange);
+
+                            Render.Circle.DrawCircle(ObjectManager.Player.Position, Champ.Item("Range" + slot.Key + Player.ChampionName).GetValue<Slider>().Value, Champ.Item("Color" + slot.Key + Player.ChampionName).GetValue<Color>(), Champ.Item("Width" + slot.Key + Player.ChampionName).GetValue<Slider>().Value);
+                            line.Draw(drawColor,lineWidth);
+                            var currentAngel = coneAngel * (float) Math.PI / 180;
+                            var conePoint1 = playerPosition + abilRange * direction.Rotated(currentAngel);
+                            currentAngel = (360-coneAngel) * (float) Math.PI / 180;
+                            var conePoint2 = playerPosition + abilRange * direction.Rotated(currentAngel);
                         }
                         break;
 
@@ -119,7 +130,7 @@ namespace Mata_Indicator
             }
         }
 
-
+2
         public static void MenuHandlerRun()
         {
             Menu.AddSubMenu(Champ);
