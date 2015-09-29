2a64c6574643692de7262c0635c1c3457fe176a6
werdbrian <werdbrian@gmail.com>
2015-09-28 20:24:07 -0700

    Update Program.cs

--- Mata Indicator/Mata Indicator/Program.cs
+++ Mata Indicator/Mata Indicator/Program.cs
@@ -100,10 +100,10 @@ namespace Mata_Indicator
                              var direction = ObjectManager.Player.Direction.To2D().Perpendicular();
                             Render.Circle.DrawCircle(ObjectManager.Player.Position, Champ.Item("Range" + slot.Key + Player.ChampionName).GetValue<Slider>().Value, Champ.Item("Color" + slot.Key + Player.ChampionName).GetValue<Color>(), Champ.Item("Width" + slot.Key + Player.ChampionName).GetValue<Slider>().Value);
                             line.Draw(drawColor,lineWidth);
-                            var currentAngel = coneAngle * (float) Math.PI / 180;
-                            var conePoint1 = ObjectManager.Player.Position + abilRange * direction.Rotated(currentAngel);
-                            currentAngel = (360-coneAngle) * (float) Math.PI / 180;
-                            var conePoint2 = ObjectManager.Player.Position + abilRange * direction.Rotated(currentAngel);
+                      //      var currentAngel = coneAngle * (float) Math.PI / 180;
+           //                 var conePoint1 = ObjectManager.Player.Position + abilRange * direction.Rotated(currentAngel);
+                         //  currentAngel = (360-coneAngle) * (float) Math.PI / 180;
+                         //   var conePoint2 = ObjectManager.Player.Position + abilRange * direction.Rotated(currentAngel);
                         }
                         break;
 
